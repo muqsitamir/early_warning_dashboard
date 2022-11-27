b34373090bd2511441eb35b7cc3056c84e852469
Saad Mehmoon <47217376+saadmehmoon@users.noreply.github.com>
2022-11-28 03:04:38 +0500

    Added Archive/Star functionality and event tabs

--- src/features/filters/filterSlice.js
+++ src/features/filters/filterSlice.js
@@ -1,47 +1,57 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 import dateRange from "react-date-range/dist/components/DateRange";
 
 export const filterSlice = createSlice({
-  name: 'filters',
+  name: "filters",
   initialState: {
     filterApplied: false,
     range: {
       startDate: null,
       endDate: new Date(),
-      key: 'range'
+      key: "range",
     },
     startTime: new Date(),
     endTime: new Date(),
     cameras: [],
-    species: []
+    species: [],
   },
   reducers: {
-      setTimeRange: (state, action) => {
-          debugger;
+    setTimeRange: (state, action) => {
+      debugger;
 
-          state.startTime = action.payload.startTime;
-          state.endTime = action.payload.endTime;
-          debugger;
-      },
-      setFilterApplied: (state, action) => {
-          state.filterApplied = action.payload
-      },
-      setDateRange: (state, action) => {
-          state.range = action.payload
-      },
-      setCameras: (state, action) => {
-          state.cameras = action.payload
-      },
-      setSpecies: (state, action) => {
-          state.species = action.payload
-      },
+      state.startTime = action.payload.startTime;
+      state.endTime = action.payload.endTime;
+      debugger;
+    },
+    setFilterApplied: (state, action) => {
+      state.filterApplied = action.payload;
+    },
+    setDateRange: (state, action) => {
+      state.range = action.payload;
+    },
+    setCameras: (state, action) => {
+      state.cameras = action.payload;
+    },
+    setSpecies: (state, action) => {
+      state.species = action.payload;
+    },
+    resetFilters: (state) => {
+      state.filterApplied = false;
+      state.range = {
+        startDate: null,
+        endDate: new Date(),
+        key: "range",
+      };
+      state.startTime = new Date();
+      state.endTime = new Date();
+      state.cameras = [];
+      state.species = [];
+    },
   },
-})
+});
 
 // Action creators are generated for each case reducer function
-export const { setDateRange, setCameras, setSpecies, setFilterApplied, setTimeRange } = filterSlice.actions
+export const { setDateRange, setCameras, setSpecies, setFilterApplied, setTimeRange, resetFilters } = filterSlice.actions;
 export const selectFilters = (state) => state.filters;
 
-
-export default filterSlice.reducer
-
+export default filterSlice.reducer;
