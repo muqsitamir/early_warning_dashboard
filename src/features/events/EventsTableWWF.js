94e21229b98c5b81127f3ef371bbbdcfbe4281b7
muqsitamir <islammuqsit7@gmail.com>
2023-12-11 14:38:37 +0500

    adding build (Giving grid and page size to WWF)

--- src/features/events/EventsTableWWF.js
+++ src/features/events/EventsTableWWF.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useState, useRef} from "react";
 import {
-    Button,
-    Chip,
+    Box,
+    Button, Checkbox,
+    Chip, Grid,
     Paper, Tab,
     Table,
     TableBody,
@@ -16,8 +17,16 @@ import {getEvents, resetEvents, selectEvents} from "./eventsSlice";
 import {selectFilters, setFilterApplied} from "../filters/filterSlice";
 import GridViewIcon from "@mui/icons-material/GridView";
 import ReorderIcon from "@mui/icons-material/Reorder";
+import UnarchiveIcon from "@mui/icons-material/Unarchive";
+import ArchiveIcon from "@mui/icons-material/Archive";
+import StarIcon from "@mui/icons-material/Star";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export function EventsTableWWF(){
+    const [listView, setListView] = useState(true);
+    const isFirstRun = useRef(true);
+    const isFirstRender = useRef(true);
+    const justRan = useRef(false);
     const [tab, setTab] = useState(0);
     const prevTab = useRef(tab);
     const [state, setState] = useState({page: 0, rowsPerPage: 10});
@@ -36,17 +45,41 @@ export function EventsTableWWF(){
         }
       };
 
+    const {page, rowsPerPage} = state;
+
     useEffect(() => {
-        if(prevTab != tab){
+        if(prevTab != tab) {
             prevTab.current = tab;
             dispatch(resetEvents());
-            setState({ page: 0, rowsPerPage: 10 });
+            setState({page: 0, rowsPerPage: 10});
         }
         dispatch(getEvents(state.page + 1, filters.filterApplied, status(tab)));
+    }, [tab])
+
+    useEffect(() => {
+        if (isFirstRun.current) {
+            isFirstRun.current = false;
+            return;
+        }
+        dispatch(resetEvents());
+        dispatch(getEvents(state.page + 1, filters.filterApplied, status(tab), rowsPerPage));
+      }, [rowsPerPage]);
+
+    useEffect(() => {
+        if (isFirstRender.current) {
+          isFirstRender.current = false;
+          return;
+        }
+        if (justRan.current) {
+          justRan.current = false;
+          return;
+        }
+        dispatch(resetEvents());
+        dispatch(getEvents(state.page + 1, filters.filterApplied, status(tab), rowsPerPage));
+        justRan.current = true;
         let check = filters.filterApplied ? false : filters.filterApplied;
         dispatch(setFilterApplied(check));
-    }, [filters, tab])
-
+        }, [filters]);
 
       const handleChangePage = (event, newPage) => {
         if (newPage > state.page && newPage + 1 > events.length/rowsPerPage) dispatch(getEvents(newPage + 1, filters.filterApplied, status(tab), rowsPerPage));
@@ -56,75 +89,88 @@ export function EventsTableWWF(){
     const handleChangeRowsPerPage = (event) => {
         setState({rowsPerPage: parseInt(event.target.value, 10), page: 0});
     };
-    const {page, rowsPerPage} = state;
+    debugger
     return (
-            <Paper className="mb4">
-                <TableContainer style={{maxHeight: 1200}}>
-                    <Tabs value={tab} onChange={(e, v) => setTab(v)} aria-label="basic tabs example" sx={{ flex: 9.3 }}>
-                        <Tab label="All" />
-                        <Tab label="Featured" />
-                    </Tabs>
-                    <Table size="small" stickyHeader aria-label="sticky table">
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Event</TableCell>
-                                <TableCell>Specie</TableCell>
-                                <TableCell>Created At</TableCell>
-                                <TableCell>Updated At</TableCell>
-                                <TableCell>Date</TableCell>
-                                <TableCell>Camera</TableCell>
-                            </TableRow>
-                        </TableHead>
+        <Paper className="mb4">
+            <TableContainer style={{maxHeight: 1200}}>
+                <Box sx={{ borderBottom: 1, borderColor: "divider", display: "flex" }}>
+              <Tabs value={tab} onChange={(e, v) => setTab(v)} aria-label="basic tabs example" sx={{ flex: 9.3 }}>
+                <Tab label="All" />
+                <Tab label="Featured" />
+                {listView ? <GridViewIcon sx={{ position: 'absolute', right: 15, top: 12, color: "#1a76d2", '&:hover': {boxShadow: '0 0 5px 2px skyblue'} }} onClick={() => setListView(!listView)} /> : <ReorderIcon sx={{ position: 'absolute', right: 15, top: 12, color: "#1a76d2", '&:hover': {boxShadow: '0 0 5px 2px skyblue'} }} onClick={() => setListView(!listView)}/>}
+              </Tabs>
+            </Box>
+            {listView ?
+                 // List View
+              (<Table size="small" stickyHeader aria-label="sticky table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>
+                  </TableCell>
+                  <TableCell>Event</TableCell>
+                  <TableCell>Specie</TableCell>
+                  <TableCell>Created At</TableCell>
+                  <TableCell>Confidence</TableCell>
+                  <TableCell>Date</TableCell>
+                  <TableCell>Camera</TableCell>
+                </TableRow>
+              </TableHead>
 
-                        {   events.length !== 0 ?
-                            <TableBody>{(rowsPerPage > 0
-                                    ? events.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                    : events
-                            ).map((row) => {
-                                // debugger;
-                                return <TableRow key={row.uuid}>
-                                    <TableCell>
-                                        <a target="_blank" href={row.file}>
-                                        <img
-                                            src={row.thumbnail}
-                                            height={80}/>
-                                        </a>
-                                    </TableCell>
-                                    <TableCell>
-                                        {row.species.map((item) => (
-                                            <Chip
-                                                className="mr1"
-                                                style={{backgroundColor: item.color}}
-                                                color="primary"
-                                                key={item.key}
-                                                label={item.name}
-                                            />))}
-                                    </TableCell>
-                                    <TableCell>
-                                        {row.created_at}
-                                    </TableCell>
-                                    <TableCell>
-                                        {row.updated_at}
-                                    </TableCell>
-                                    <TableCell>
-                                        {row.date}
-                                    </TableCell>
-                                    <TableCell>
-                                        {row.camera_name}
-                                    </TableCell>
-                                </TableRow>
-                            })}</TableBody> :
-                            <div className="container tc">Loading Data....</div>}
-                    </Table>
-                </TableContainer>
-                <TablePagination
-                    rowsPerPageOptions={[]}
-                    component="div"
-                    count={count !== null ? count : "loading..."}
-                    rowsPerPage={rowsPerPage}
-                    page={page}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                    onPageChange = {handleChangePage}/>
-            </Paper>
-        );
+              {events.length !== 0 ? (
+                <TableBody>
+                  {(rowsPerPage > 0 ? events.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) : events).map((row) => {
+                    return (
+                      <TableRow key={row.uuid}>
+                        <TableCell>
+                        </TableCell>
+                        <TableCell>
+                          <a target="_blank" href={row.file}>
+                            <img src={row.thumbnail} height={80} />
+                          </a>
+                        </TableCell>
+                        <TableCell>
+                          {row.species.map((item) => (
+                            <Chip className="mr1" style={{ backgroundColor: item.color }} color="primary" key={item.key} label={item.name} />
+                          ))}
+                        </TableCell>
+                        <TableCell>{row.created_at}</TableCell>
+                        <TableCell>{row.confidence}</TableCell>
+                        <TableCell>{row.date}</TableCell>
+                        <TableCell>{row.camera_name}</TableCell>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              ) : (
+                <div className="container tc">Loading Data....</div>
+              )}
+            </Table>)
+                :
+            (
+             <div>
+              <Grid container sx={{paddingTop: 3, paddingLeft: 5, paddingRight: 5}} spacing={{ xs: 2, md: 1 }} columns={{ xs: 4, sm: 8, md: 14 }}>
+              {(rowsPerPage > 0 ? events.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage) : events).map((row, index) => (
+                <Grid item xs={2} sm={4} md={2} key={index} >
+                  <a target="_blank" href={row.file} style={{marginTop: 20 }}>
+                     <img style={{ border: "groove", borderColor: "gray", borderRadius: 10, marginBottom: 2 }} src={row.thumbnail} height={100} />
+                  </a>
+                </Grid>
+              ))}
+            </Grid>
+          </div>
+            )
+            }
+          </TableContainer>
+          <TablePagination
+            rowsPerPageOptions={[10, 20, 50, 100]}
+            component="div"
+            count={count !== null ? count : "loading..."}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            onRowsPerPageChange={handleChangeRowsPerPage}
+            onPageChange={handleChangePage}
+            showFirstButton
+          />
+    </Paper>
+    );
 }
