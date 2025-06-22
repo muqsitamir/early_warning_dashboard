a73f03c01dc7d4af17e2712aed936768e5e254c7
muqsitamir <islammuqsit7@gmail.com>
2025-06-23 02:32:44 +0500

    Corrected dropdown menu css

--- src/Headers/TopAppBar/TopAppBar.js
+++ src/Headers/TopAppBar/TopAppBar.js
@@ -1,43 +1,36 @@
-import React,{useState} from "react";
+import React, {useState} from "react";
+import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-//import MenuIcon from '@mui/icons-material/Menu';
-//import { showSideNav } from '../../reusable_components/site_data/siteDataSlice'
 import './style.css'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { useDispatch } from "react-redux";
 
 export default function TopAppBar(){
-   // const dispatch = useDispatch()
-    /*const handle_side_nav = () => {
-        dispatch(showSideNav());
-    }
-*/
-const user = JSON.parse(localStorage['user']);
-console.log("organization: " + user.organization);
-const username=user.first_name+" "+user.last_name;
-const organization = user.organization;
-const dispatch = useDispatch();
-const [dropdownOpen, setDropdownOpen] = useState(false);
+    const user = JSON.parse(localStorage['user']);
+    const username=user.first_name+" "+user.last_name;
+    const organization = user.organization;
+
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+
     const handle_camera_click = () => {
         window.location = '/cameras';
     }
- const handle_profile_click=()=>{
-    window.location="/profile";
- }
- const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen);
-};
-const handle_admin_click = () => {
-    window.location = 'https://api.tpilums.org.pk/admin';
-};
 
-const handleChangePassword = () => {
-    window.location = '/change-password';
-};
+    const toggleDropdown = () => {
+        setDropdownOpen(!dropdownOpen);
+        debugger;
+    };
 
-const handleLogout = () => {
-    window.location = '/logout';
-};
+    const handle_admin_click = () => {
+        window.location = 'https://api.tpilums.org.pk/admin';
+    };
+
+    const handleChangePassword = () => {
+        window.location = '/change-password';
+    };
+
+    const handleLogout = () => {
+        window.location = '/logout';
+    };
 
     return (
         <header  style={{ background: 'white' }} className="mdc-top-app-bar mdc-top-app-bar--shadow__ mdc-top-app-bar--fixed js-top-app-bar"
@@ -52,27 +45,28 @@ const handleLogout = () => {
                         <img width='50' src={require("../../images/wwf_logo.png")} alt="Project Logo"/>
                     </Link>
                 </section>
-                <section className="mdc-top-app-bar__section app-shop mdc-top-app-bar__section--align-end mr4"
-                         role="toolbar">
+                <section
+                 className="mdc-top-app-bar__section app-shop mdc-top-app-bar__section--align-end mr4"
+                 role="toolbar"
+                 style={{ position: 'relative' }}
+                >
                     <Link to="/" className="show-lg link-mute">
                         <button className="mdc-button mdc-theme--primary mdc-top-app-bar__action-item" >
                             <span className="mdc-button__ripple"/>
                             <span className="mdc-button__label">Home</span>
                         </button>
                     </Link>
-                    {/*<Link to="/cameras" className="show-lg link-mute">*/}
                         <button className="mdc-button mdc-theme--primary mdc-top-app-bar__action-item" onClick={handle_camera_click}>
                             <span className="mdc-button__ripple"/>
                             <span className="mdc-button__label">Cameras</span>
                         </button>
 
-                    {/*</Link>*/}
                     <button className="mdc-button mdc-theme--primary mdc-top-app-bar__action-item profile-button" onClick={toggleDropdown}>
-                        <AccountCircleIcon style={{ fontSize: 40 }} /> {/* Use the icon */}
+                        <AccountCircleIcon style={{ fontSize: 40 }} />
                     </button>
                     
-                    {dropdownOpen && (
-                        <div className="dropdown-menu1" style={{display:'flex !important'}}>
+                    {dropdownOpen ? (
+                        <div className="dropdown-menu1" >
                             <span style={{color:'black',fontWeight:'700',textAlign:'center'}}>{username}( <sub>{organization}</sub>)</span>
                             <br/>
                             {organization === "CVGL" && (
@@ -81,13 +75,12 @@ const handleLogout = () => {
                             <button onClick={handleChangePassword}>Change Password</button>
                             <button onClick={handleLogout}>Log out</button>
                         </div>
-                    )}
+                    ) : (<></>)}
                     <button
                         className="menu-mobile material-icons mdc-theme--primary mdc-top-app-bar__navigation-icon mdc-icon-button dn-l js-trigger-mdc-drawer"
                         aria-label="Open navigation menu">menu
                     </button>
                 </section>
-
             </div>
         </header>
     );
